46603beafc9141a393ecf3224964b2aa6c121a3b
miw-upm <miw@eui.upm.es>
2014-09-27 13:15:08 +0200

    factory method

---
+++ src/main/java/es/upm/miw/pd/factoryMethod/Product.java
@@ -0,0 +1,5 @@
+package es.upm.miw.pd.factoryMethod;
+
+public abstract class Product {
+    public abstract String view();
+}
---
+++ src/main/java/es/upm/miw/pd/factoryMethod/Product1.java
@@ -0,0 +1,8 @@
+package es.upm.miw.pd.factoryMethod;
+
+public class Product1 extends Product {
+    @Override
+    public String view() {
+        return "Product1...";
+    }
+}
