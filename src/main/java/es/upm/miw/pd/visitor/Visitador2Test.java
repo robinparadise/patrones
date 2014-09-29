36b73c07f69d7bf41bcec0b7781a26b4285cc9a7
miw-upm <miw@eui.upm.es>
2014-09-29 19:07:18 +0200

    visitor

---
+++ src/main/java/es/upm/miw/pd/visitor/Visitador2Test.java
@@ -0,0 +1,41 @@
+package es.upm.miw.pd.visitor;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Visitador2Test {
+    private Collection<element> coleccion = new ArrayList<element>();
+
+    @Before
+    public void ini() {
+        coleccion.add(new ElementA());
+        coleccion.add(new ElementA());
+        coleccion.add(new ElementB());
+        coleccion.add(new ElementA());
+        coleccion.add(new ElementB());
+    }
+
+    @Test
+    public void testVisitorAs() {
+        Visitor2 v2 = new Visitor2();
+        for (element elemento : coleccion) {
+            elemento.accept(v2);
+        }
+        assertEquals(3, v2.getAs());
+    }
+
+    @Test
+    public void testVisitorBs() {
+        Visitor2 v2 = new Visitor2();
+        for (element elemento : coleccion) {
+            elemento.accept(v2);
+        }
+        assertEquals(2, v2.getBs());
+    }
+
+}
---
+++ src/main/java/es/upm/miw/pd/visitor/Visitor2.java
@@ -0,0 +1,29 @@
+package es.upm.miw.pd.visitor;
+
+public class Visitor2 extends Visitor {
+    private int as = 0, bs = 0;
+
+    protected int getAs() {
+        return as;
+    }
+
+    protected int getBs() {
+        return bs;
+    }
+
+    @Override
+    public void visitElementA(ElementA e) {
+        as++;
+    }
+
+    @Override
+    public void visitElementB(ElementB e) {
+        bs++;
+    }
+
+    @Override
+    public String toString() {
+        return "As:" + as + ", Bs:" + bs;
+    }
+
+}
